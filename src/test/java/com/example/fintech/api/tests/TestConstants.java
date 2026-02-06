e7e01a2252ac4c2cc05a9afce8656a81a51946cc
krasimirkuzmanov <krasimir.kuzmanov@gmail.com>
2026-02-06 16:51:16 +0200

    fix: Commit TestConstants class

---
+++ src/test/java/com/example/fintech/api/tests/TestConstants.java
@@ -0,0 +1,8 @@
+package com.example.fintech.api.tests;
+
+public final class TestConstants {
+
+  public static final String ERROR_CODE_USER_ALREADY_EXISTS = "USER_ALREADY_EXISTS";
+  public static final String ERROR_CODE_INVALID_CREDENTIALS = "INVALID_CREDENTIALS";
+
+}
